034b4d597a910a93461113e9d7f8ff1af1d4a79b
7oda <mahmoud.abdo2772@gmail.com>
2025-08-07 00:35:12 +0300

    forgetPass-page done

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:learnfy/core/theme/app_theme.dart';
-import 'features/auth/presentation/views/sign_up_page.dart';
+import 'package:learnfy/features/auth/presentation/views/forget_pass_view.dart';
 import 'package:learnfy/features/auth/presentation/views/on_boarding_view.dart';
+import 'features/auth/presentation/views/sign_up_page.dart';
 
 void main() {
-  runApp(const Learnfy());
+  runApp(const MyApp());
 }
-
-
-class Learnfy extends StatelessWidget {
-  const Learnfy({super.key});
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +17,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightMode,
       home: Scaffold(
-        body: SignUpPage(),
-      home: const OnboardingView(),
+        body: ForgetPasswordView(),
+      ),
     );
   }
 }
-
